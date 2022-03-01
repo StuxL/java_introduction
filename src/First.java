687123f2ff42b812b3d5138cdd0502aa061aae04
StuxL <43639597+StuxL@users.noreply.github.com>
2022-03-01 13:27:40 +1100

    Java Basics

---
+++ src/First.java
@@ -0,0 +1,16 @@
+public class First {
+
+  public static void main(String[] args) {
+    //Primatives
+    int a = 5;
+    float b = 3.4;
+    boolean c = false;
+
+    final int MAX_HEIGHT = 100; //Constant
+
+    //Casting and assignment
+    b = a; //OK
+    a = (int) b //Force narrowing conversion with case
+    System.out.println()
+  }
+}
